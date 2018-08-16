7931622b4cb6c2b13901e6b01b8abdb5c4be43bf
Steve White <stephenkrohnwhite@gmail.com>
2018-08-16 11:17:58 -0500

    implementing jquery functionality

--- BandDrop/Scripts/pusher.js
+++ BandDrop/Scripts/pusher.js
@@ -1,17 +1,1 @@
 ﻿
-  <script src="https://js.pusher.com/4.2/pusher.min.js"></script>
-  <script>
-
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
-
-    var pusher = new Pusher('de9851a0744ee8227c97', {
-      cluster: 'us2',
-      encrypted: true
-    });
-
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-      alert(JSON.stringify(data));
-    });
-  </script>
