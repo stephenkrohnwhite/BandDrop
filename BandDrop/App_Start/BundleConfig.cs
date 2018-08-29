b5e44e39d43e0c4112dff83d423fbfb0631cf9ab
Steve White <stephenkrohnwhite@gmail.com>
2018-08-28 21:02:04 -0500

    stylized chat display. addin mediaelement.js to media player

--- BandDrop/App_Start/BundleConfig.cs
+++ BandDrop/App_Start/BundleConfig.cs
@@ -13,7 +13,8 @@ namespace BandDrop
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
+            bundles.Add(new ScriptBundle("~/bundles/mediaelement").Include(
+                "~/Scripts/mediaelement/mediaelement-and-player.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
